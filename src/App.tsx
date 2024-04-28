f059484f602184a921d7fbfc62371ecaa7b5b200
zahid mushtaq <mushtaqzahid888@gmail.com>
2024-04-28 20:43:41 +0530

    Added shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import { Button } from '@/components/ui/button'
 function App() {
 
   return (
     <>
       <h1>Welcome to ellib-admin-dashboard!</h1>
+      <Button>Click Me</Button>
     </>
   )
 }
